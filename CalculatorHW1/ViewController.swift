e0fc72b647f1cd44be75e8c12868866b3443db38
Jinglin Liu <liujinglinlele@hotmail.com>
2017-09-13 09:49:00 -0700

    Initial Commit

---
+++ CalculatorHW1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorHW1
+//
+//  Created by Jinglin Liu on 9/13/17.
+//  Copyright © 2017 Jinglin Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
